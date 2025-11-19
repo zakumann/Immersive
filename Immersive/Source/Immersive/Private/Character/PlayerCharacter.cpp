2766be0ff647d0e1e468f5f4eac45f50999484c4
zakumann <samuel742able@gmail.com>
2025-11-19 17:16:59 +0900

    PlayerController update

--- Immersive/Source/Immersive/Private/Character/PlayerCharacter.cpp
+++ Immersive/Source/Immersive/Private/Character/PlayerCharacter.cpp
@@ -2,6 +2,12 @@
 
 
 #include "Character/PlayerCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -9,6 +15,25 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    // Player Height and Collision
+    GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+    // First Person Camera
+    FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+    // Attach the camera to the capsule so it moves/rotates with the character
+    FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+
+    // Put the camera at eye height (Z)
+    FirstPersonCameraComponent->SetRelativeLocation(FVector(0.f, 0.f, 64.f)); // tweak to taste
+    FirstPersonCameraComponent->bUsePawnControlRotation = true; // camera follows controller rotation
+
+    // Make the character rotate with controller (for FPS)
+    bUseControllerRotationYaw = true;
+    bUseControllerRotationPitch = true;
+    bUseControllerRotationRoll = false;
+
+    // Let movement not rotate the character automatically (we handle via controller)
+    GetCharacterMovement()->bOrientRotationToMovement = false;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +41,13 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(PlayerMappingContext, 0); // Add your IMC
+        }
+    }
 }
 
 // Called every frame
@@ -30,5 +62,54 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        // Bind Move
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
+
+        // Bind Look
+        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMyCharacter::Look);
+
+        // Bind Jump
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AMyCharacter::JumpPressed);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &AMyCharacter::JumpReleased);
+    }
 }
 
+void Move(const FInputActionValue& Value)
+{
+    // Input is a Vector2D
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    if (Controller != nullptr)
+    {
+        // find out which way is forward
+        const FRotator Rotation = Controller->GetControlRotation();
+        const FRotator YawRotation(0, Rotation.Yaw, 0);
+        // get forward vector
+        const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+        // get right vector 
+        const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+        // add movement 
+        AddMovementInput(ForwardDirection, MovementVector.Y);
+        AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+void Look(const FInputActionValue& Value)
+{
+    // Input is a Vector2D
+    FVector2D LookAxisVector = Value.Get<FVector2D>();
+    if (Controller != nullptr)
+    {
+        // add yaw and pitch input to controller
+        AddControllerYawInput(LookAxisVector.X);
+        AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+void JumpPressed()
+{
+	Jump();
+}
+void JumpReleased()
+{
+	StopJumping();
+}
--- Immersive/Source/Immersive/Public/Character/PlayerCharacter.h
+++ Immersive/Source/Immersive/Public/Character/PlayerCharacter.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
 
 UCLASS()
 class IMMERSIVE_API APlayerCharacter : public ACharacter
@@ -26,4 +31,27 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+    UCameraComponent* FirstPersonCameraComponent;
+
+    // Input Action references
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+    UInputAction* IA_Move;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+    UInputAction* IA_Look;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+    UInputAction* IA_Jump;
+
+    // Input Mapping Context reference
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+    UInputMappingContext* DefaultMappingContext;
+
+    // Input handling functions
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
+    void JumpPressed(); // For Digital Input Actions
+    void JumpReleased();
 };
